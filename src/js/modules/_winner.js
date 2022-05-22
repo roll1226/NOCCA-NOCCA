eb5e251e1878378874265e4bcce12a4811f5fe40
roll1226 <tomo_yasu_4724@icloud.com>
2022-05-22 21:50:51 +0900

    fix: winner

--- src/js/modules/_winner.js
+++ src/js/modules/_winner.js
@@ -1,16 +1,18 @@
 import { WHITE } from "./_playGame";
 
 export const winnerAnimation = (player) => {
-  document.querySelector(".main").style.display = "none";
-  document.querySelector("#camera-horizontal").style.display = "none";
+  setTimeout(() => {
+    document.querySelector(".main").style.display = "none";
+    document.querySelector("#camera-horizontal").style.display = "none";
 
-  let playerName = "";
-  if (player === WHITE) {
-    playerName = "白駒の勝ち";
-  } else {
-    playerName = "黒駒の勝ち";
-  }
+    let playerName = "";
+    if (player === WHITE) {
+      playerName = "白駒の勝ち";
+    } else {
+      playerName = "黒駒の勝ち";
+    }
 
-  document.querySelector(".winner__player--container").dataset.show = "true";
-  document.querySelector(".winner__player").innerHTML = playerName;
+    document.querySelector(".winner__player--container").dataset.show = "true";
+    document.querySelector(".winner__player").innerHTML = playerName;
+  }, 300);
 };
