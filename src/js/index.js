12f49db438be249f9b9ef3b7a4ad47d7e5ed21c9
roll1226 <tomo_yasu_4724@icloud.com>
2022-05-22 16:55:04 +0900

    feat: top animation

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "./modules/_main";
 import "./modules/_camera";
+import "./modules/_start";
 import "@scss/style.scss";
 import "reset-css/reset.css";
---
+++ src/js/modules/_start.js
@@ -0,0 +1,8 @@
+console.log("start!!!!!!!!");
+const startButton = document.querySelector(".start");
+startButton.addEventListener("click", () => {
+  document.querySelector(".top").classList.add("top--close");
+
+  document.querySelector("#camera-horizontal").classList.add("start--game");
+  document.querySelector(".main").classList.add("start--game");
+});
